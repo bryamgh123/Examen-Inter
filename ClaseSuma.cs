f2124c0d287c934b730526efeddab642f21bede4
Pepe <95196085+PePeWee07@users.noreply.github.com>
2022-11-01 19:39:17 -0500

    Correccion de la clase suma

--- ClaseSuma.cs
+++ ClaseSuma.cs
@@ -12,7 +12,7 @@ public class ClaseSuma
     public global::System.Int32 Num1 { get => num1; set => num1 = value; }
     public global::System.Int32 Num2 { get => num2; set => num2 = value; }
 
-    public int suma()
+    public int sumas()
     {
         return num1 + num1;
     }
