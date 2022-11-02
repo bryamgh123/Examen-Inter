8621373fb88346d7f9e7c09040d238dcb37dff95
Pepe <95196085+PePeWee07@users.noreply.github.com>
2022-11-01 19:28:46 -0500

    Se creo la clase resta

---
+++ ClassResta.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class ClassResta
+
+{
+    private int num1;
+    private int num2;
+    public ClassResta()
+    {
+    }
+
+    public global::System.Int32 Num1 { get => num1; set => num1 = value; }
+    public global::System.Int32 Num2 { get => num2; set => num2 = value; }
+
+    public int Rta()
+    {
+        return num1 - num1;
+    }
+
+}
